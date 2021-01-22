338be709b086465535372aae36a0174d93964594
stebs <estebancazaldoutreleau@gmail.com>
2021-01-21 22:59:50 -0400

    props added

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,29 @@
 import React from 'react' 
+import excersiseImg from '../images/exercise.png'
+import circles from '../images/circles.png'
+import './styles/Card.css'
 
 class Card extends React.Component {
     render(){
-       return <h1>Card</h1>
+        const {title, description, img, leftColor, rightColor} = this.props
+       return (
+           <div className="card mx-auto Fitness-Card" style={{
+               backgroundImage: `url(${circles}), linear-gradient(to right, ${leftColor}, ${rightColor} )`
+           }}>
+               <div className="card-body">
+                    <div className="row center">
+                        <div className="col-6">
+                            <img src={img} className="float-right" />
+                        </div>
+                        <div className="col-6 Fitness-Card-Info">
+                            <h1>{title}</h1>
+                               <p> {description} </p>
+                        </div>
+                    </div>
+               </div>
+           </div>
+           
+       )
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react' 
 import ReactDOM from 'react-dom' 
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
-ReactDOM.render(<Card />, container)
+ReactDOM.render(<Card 
+            title = "Technique guides of workout"
+            description = "Learn how to workout and results quickly"
+            img = "https://raw.githubusercontent.com/rulpalacios/FitnessApp/styles/src/images/exercise.png"
+            leftColor = "#a97dcc"
+            rightColor = "#617BFB"
+/>, container)
