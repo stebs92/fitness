a84b59ad132acd24bea00998365c5c27016770ac
stebs <estebancazaldoutreleau@gmail.com>
2021-01-28 16:42:42 -0400

    add iteration & button2 components

---
+++ src/components/Button2.js
@@ -0,0 +1,12 @@
+import React from 'react' 
+import './styles/Button2.css'
+
+function Button2 (){
+    return(
+        <div className="text-center m-5 button2">
+            <img src="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/add.png?alt=media&token=fbe1d973-4b4f-42d8-b76e-2c4354f6ed9d" alt="button" />
+        </div>
+    )
+}
+
+export default Button2
